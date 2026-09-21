1e9b7d3c767f0dcf9418c912b6a12be578ab3b2f
BKarpow <xymerone@gmail.com>
2026-09-21 22:38:01 +0300

    Testttt10

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\Admin\ProductController as AdminProductController;
 use App\Http\Controllers\Admin\CategoryController as AdminCategoryController;
 use App\Http\Controllers\Admin\BrandController as AdminBrandController;
 use App\Http\Controllers\Admin\DashboardController;  
-use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\UserController;  
 use App\Http\Controllers\Auth\SocialiteController;
 use App\Http\Controllers\CartController;
 
